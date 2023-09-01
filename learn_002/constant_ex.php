2511ea05c2f69ca0f30f5be2ca91fd281cae8be7
anobhask <anobhask@gmail.com>
2023-09-01 14:21:39 +0530

    constant usage

---
+++ learn_002/constant_ex.php
@@ -0,0 +1,12 @@
+#! /usr/bin/php
+
+<?php
+//define keyword for constant
+// constant are written in capiptals
+// No need to put '$' infront of constant
+define('WIDTH',400); 
+const LENGTH = "200";
+echo WIDTH, LENGTH . "\n";
+print WIDTH * LENGTH . "\n";
+ 
+?>
